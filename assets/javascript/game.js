d421cec30f8a7a2251247d3203a148f02bf06adc
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-07-08 18:28:37 -0400

    sample questions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,24 @@
-
+const trivia = [{
+    question: "Which molecule has been referred to as the 'Spirit Molecule' for its ability to induce intense mystical experiences?",
+    choices: ["N,N-Dimethyltryptamine", "Lysergic acid diethylamide", "Tetrahydrocannabinol", "3,4-Methylenedioxymethamphetamine"],
+    answer: "N,N-Dimethyltryptamine"
+},{
+    question: "Who is the 'Father of LSD' & author of 'LSD: My Problem Child'?",
+    choices: ["Timothy Leary", "Alexander Shulgin", "William Leonard Pickard", "Albert Hoffman"],
+    answer: "Albert Hoffman"
+},{
+    question: "Which naturally occuring psychedelic was discovered in 1956 and had its active molecule isolated in 1958 by Albert Hoffman?",
+    choices: ["Ayuhasca", "Psilocybe Cubensis", "Peyote", "Bufo Alvarius Toad Venom"],
+    answer: "Psilocybe Cubensis"
+},{
+    question: "Which is a quote from 'Fear and Loathing in Las Vegas'?",
+    choices: ["'Frankly, my dear, I don't give a damn.'", "'I'm as mad as hell, and I'm not going to take this anymore!'", "'We can't stop here, this is bat country!'", "'You're gonna need a bigger boat.'"],
+    answer: "'We can't stop here, this is bat country!'"
+},{
+    question: "Which 1969 music festival was host to over 500,000 attendees?",
+    choices: ["Woodstock", "Burning Man", "Coachella", "Bonnaroo"],
+    answer: "Woodstock"
+}];
 
 let game = {
 
